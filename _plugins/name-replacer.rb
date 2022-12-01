70791a446ed9938d78c5cf73d84a0e6ae28a2062
ShaWn <nonyabizness@gmail.com>
2022-11-30 21:20:25 -0600

    Using dynamic data for pokemon names.

--- _plugins/name-replacer.rb
+++ _plugins/name-replacer.rb
@@ -1,4 +1,8 @@
 # Plugins are disabled on Github Pages... :(
 Jekyll::Hooks.register([:pages, :posts], :pre_render) do |post|
-    post.content = post.content.gsub('Misdreavus', '[Misdreavus](https://bulbapedia.bulbagarden.net/wiki/Misdreavus_(Pok%C3%A9mon)){:target="_blank"}')
+    names = Array.[]("Misdreavus", "Mismagius")
+    names.each {
+        |x| post.content = post.content.gsub(x, "[#{x}](https://bulbapedia.bulbagarden.net/wiki/#{x}_(Pok%C3%A9mon)){:target=\"_blank\"}")
+    }
+    
 end
