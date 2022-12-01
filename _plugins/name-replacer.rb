121c2cb3bc8ec545d2b4f681578d2f9cf8af0f04
ShaWn <nonyabizness@gmail.com>
2022-11-30 21:13:29 -0600

    Opening Pokemon links in a new window.

--- _plugins/name-replacer.rb
+++ _plugins/name-replacer.rb
@@ -1,4 +1,4 @@
 # Plugins are disabled on Github Pages... :(
 Jekyll::Hooks.register([:pages, :posts], :pre_render) do |post|
-    post.content = post.content.gsub('Misdreavus', '[Misdreavus](https://bulbapedia.bulbagarden.net/wiki/Misdreavus_(Pok%C3%A9mon))')
+    post.content = post.content.gsub('Misdreavus', '[Misdreavus](https://bulbapedia.bulbagarden.net/wiki/Misdreavus_(Pok%C3%A9mon)){:target="_blank"}')
 end
