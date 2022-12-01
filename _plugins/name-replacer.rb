b630365e07023093efa9c7b850a75748856c80c0
ShaWn <nonyabizness@gmail.com>
2022-11-30 22:14:43 -0600

    Fixing bug in name replacer.

--- _plugins/name-replacer.rb
+++ _plugins/name-replacer.rb
@@ -5,7 +5,7 @@ Jekyll::Hooks.register([:pages, :posts], :pre_render) do |post|
         puts "Replacing!!"
         names = Array.[]("Misdreavus", "Mismagius", "Ditto")
         post.site.data['pokemon_names'].each {
-            |x| post.content = post.content.gsub(x['Name'], "[#{x['Name']}](https://bulbapedia.bulbagarden.net/wiki/#{x['Pokemon']}_(Pok%C3%A9mon)){:target=\"_blank\"}")
+            |x| post.content = post.content.gsub(x['Name'], "[#{x['Name']}](https://bulbapedia.bulbagarden.net/wiki/#{x['Name']}_(Pok%C3%A9mon)){:target=\"_blank\"}")
         }
     end
 end
