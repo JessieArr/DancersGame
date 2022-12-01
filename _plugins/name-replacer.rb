9dfadb12d07124626e1f492fd9fb6be8962d5683
ShaWn <nonyabizness@gmail.com>
2022-11-30 22:11:07 -0600

    Switching to Pokemon names csv.

--- _plugins/name-replacer.rb
+++ _plugins/name-replacer.rb
@@ -4,8 +4,8 @@ Jekyll::Hooks.register([:pages, :posts], :pre_render) do |post|
     if post.data['link_pokemon_names']
         puts "Replacing!!"
         names = Array.[]("Misdreavus", "Mismagius", "Ditto")
-        post.site.data['pokemon'].each {
-            |x| post.content = post.content.gsub(x['Pokemon'], "[#{x['Pokemon']}](https://bulbapedia.bulbagarden.net/wiki/#{x['Pokemon']}_(Pok%C3%A9mon)){:target=\"_blank\"}")
+        post.site.data['pokemon_names'].each {
+            |x| post.content = post.content.gsub(x['Name'], "[#{x['Name']}](https://bulbapedia.bulbagarden.net/wiki/#{x['Pokemon']}_(Pok%C3%A9mon)){:target=\"_blank\"}")
         }
     end
 end
