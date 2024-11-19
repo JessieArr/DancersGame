acd96680d2a0286819cd2c9776f6624cda885090
ShaWn <nonyabizness@gmail.com>
2024-11-19 17:03:53 -0600

    Adding ability to autolink names to Stardew wiki.

--- _plugins/name-replacer.rb
+++ _plugins/name-replacer.rb
@@ -1,9 +1,17 @@
 # Plugins are disabled on Github Pages... :(
 Jekyll::Hooks.register([:pages, :posts], :pre_render) do |post|
-    # puts post.methods - Object.methods
     if post.data['link_pokemon_names']
         post.site.data['pokemon_names'].each {
             |x| post.content = post.content.gsub(x['Name'], "[#{x['Name']}](https://bulbapedia.bulbagarden.net/wiki/#{x['Name']}_(Pok%C3%A9mon)){:target=\"_blank\"}")
+        }
+    end
+end
+
+Jekyll::Hooks.register([:pages, :posts], :pre_render) do |post|
+    if post.data['link_stardew_valley_names']
+        puts "Replacing Stardew Valley Names..."
+        post.site.data['stardew_valley_names'].each {
+            |x| post.content = post.content.gsub(" " + x['Name'] + " ", " [#{x['Name']}](https://stardewvalleywiki.com/#{x['Name']}){:target=\"_blank\"} ")
         }
     end
 end
