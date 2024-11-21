f969bb7fe7c249069693644408480d7ac6fe7443
ShaWn <nonyabizness@gmail.com>
2024-11-20 22:05:48 -0600

    Updating name replacer to use Regex to identify word boundaries.

--- _plugins/name-replacer.rb
+++ _plugins/name-replacer.rb
@@ -1,17 +1,18 @@
 # Plugins are disabled on Github Pages... :(
 Jekyll::Hooks.register([:pages, :posts], :pre_render) do |post|
     if post.data['link_pokemon_names']
+        puts "Replacing Pokemon Names..." + post.data['title']
         post.site.data['pokemon_names'].each {
-            |x| post.content = post.content.gsub(x['Name'], "[#{x['Name']}](https://bulbapedia.bulbagarden.net/wiki/#{x['Name']}_(Pok%C3%A9mon)){:target=\"_blank\"}")
+            |x| post.content = post.content.gsub(/\b#{x['Name']}\b/, "[#{x['Name']}](https://bulbapedia.bulbagarden.net/wiki/#{x['Name']}_(Pok%C3%A9mon)){:target=\"_blank\"}")
         }
     end
 end
 
 Jekyll::Hooks.register([:pages, :posts], :pre_render) do |post|
     if post.data['link_stardew_valley_names']
-        puts "Replacing Stardew Valley Names..."
+        puts "Replacing Stardew Valley Names..." + post.data['title']
         post.site.data['stardew_valley_names'].each {
-            |x| post.content = post.content.gsub(" " + x['Name'] + " ", " [#{x['Name']}](https://stardewvalleywiki.com/#{x['Name']}){:target=\"_blank\"} ")
+            |x| post.content = post.content.gsub(/\b#{x['Name']}\b/, "[#{x['Name']}](https://stardewvalleywiki.com/#{x['Name']}){:target=\"_blank\"}")
         }
     end
 end
